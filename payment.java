65e4a451f62cabaf642e14bd7a6d210a4b1299b4
R Prithiviraj <itsprithiv@gmail.com>
2019-09-30 12:19:42 +0530

    changed

--- payment.java
+++ payment.java
@@ -1,12 +1,8 @@
 public class payment 
 {
 	public static void main(string args[])
-	{
-<<<<<<< HEAD
+	
 		int a=40;
-=======
-		int a=20;
->>>>>>> dev_1.2.3
 		system.out.println("payment is ");
 	}
 }
